a8118ff67e403557598b7ca4913f000c6462cba1
AnnaSanna <51660507+AnnaSanna@users.noreply.github.com>
2024-03-21 07:43:46 +1100

    Fix comments from PR

--- test/numberGame.test.js
+++ test/numberGame.test.js
@@ -1,19 +1,21 @@
 import { equal } from 'assert';
 import * as numberGame from '../numberGame.js';
 
-it('User guessed the number on the first try', () => {
-    const player = new numberGame.Guesser(12, 1);
-    equal(player.guess(12), true);
-})
+describe('User guessed the number', () => {
+    it('On the first try', () => {
+        const player = new numberGame.Guesser(12, 1);
+        equal(player.guess(12), true);
+    })
 
-it('User guessed the number on the last  try', () => {
-    const player = new numberGame.Guesser(5, 2);
-    equal(player.guess(12), false);
-    equal(player.guess(5), true);
+    it('On the last try', () => {
+        const player = new numberGame.Guesser(5, 2);
+        equal(player.guess(12), false);
+        equal(player.guess(5), true);
+    })
 })
 
 describe("Should throw an Error", () => {
-    it('Should throw an Error when there is negative number of guesses', () => {
+    it('When there is negative number of guesses', () => {
         const player = new numberGame.Guesser(5, -1);
         try {
             player.guess(5);
@@ -23,7 +25,7 @@ describe("Should throw an Error", () => {
         }
     })
 
-    it('Should throw an Error when User did not guess the number', () => {
+    it('When User did not guess the number within allocated attempts', () => {
         const player = new numberGame.Guesser(10, 3);
         equal(player.guess(1), false);
         equal(player.guess(2), false);
